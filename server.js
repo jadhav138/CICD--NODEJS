9e395ad461af1eaaccdd6fa927107df3fbc9e945
Omkar Jadhav <114341853+jadhav138@users.noreply.github.com>
2024-12-17 17:35:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
-
+require("dotenv").config()
 
 app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
-app.listen(8000,() => {
-    console.log('listening to 8000')
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"JADHAV",
+            age:22,
+            contact:######
+        },env:process.env.NAME
+    })
+});
 
+
+app.listen(process.env.PORT,() => {
+    console.log("listening to 8000");
 })
